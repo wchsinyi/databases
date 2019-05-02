5de399bd9166945d4ea9f8b7013d03e322eca4f8
wchsinyi <hsinyi.wc.uma@gmail.com>
2019-05-01 21:06:16 -0700

    Draft sql server

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,22 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  username VARCHAR(25),
+  msg VARCHAR(25), 
+  roomname VARCHAR(25),
 );
 
 /* Create other tables and define schemas for them here! */
+'INSERT INTO messages (username, msg, roomname) VALUES (body.username, body.message, body.roomname);'
 
 
 
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u student < server/schema.sql
  *  to create the database and the tables.*/
 
